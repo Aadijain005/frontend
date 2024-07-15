8b75d1350cffdf4efbb61c6d54c12ed48b8b8a94
rainy <rainyagrawaltoda@gmail.com>
2024-07-15 15:45:08 +0530

    commit4

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log(" WElcome  to Apna College");
+name="Aadi Jain";
+console.log(name);
+age=18;
+console.log(age);
+const product = {
+    title:"BAll PEN",
+    rating:4,
+    offer:5,
+    price:270,
+};
+console.log(product);
+const  profile = {
+    username:"@shradhhakhapra",
+    isFollow:false,
+    followers:123,
+    follwings:123,
+};
+
+console.log(typeof profile);
